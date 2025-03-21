1f9630e6cbd42618d9ebb973405739856e178b44
jaleef <jeffyan167@gmail.com>
2025-03-22 00:31:26 +0800

    添加了断线重连线程

--- frontEnd/src/Client.cpp
+++ frontEnd/src/Client.cpp
@@ -3,7 +3,6 @@
 #include <unistd.h>
 
 #include <cstring>
-
 #include <iostream>
 #include <string>
 #include <thread>
@@ -11,13 +10,18 @@
 constexpr int kBufferSize = 1024;
 constexpr int kPort = 8080;
 constexpr char* kServerIp = "127.0.0.1";
+constexpr int kMaxRetries = 5;
+constexpr int kCheckInterval = 2;
 
 class Client {
  public:
-  Client() = default;
+  Client() {
+    createClientSocket();
+    is_connect_ = true;
+  }
   ~Client() { close(sock); }
 
-  void connectServer() {
+  void createClientSocket() {
     struct sockaddr_in serv_addr;
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
@@ -42,33 +46,37 @@ class Client {
   }
 
   void loop() {
-    std::thread send_thread(&Client::SendThread, this);
-    std::thread recv_thread(&Client::RecvThread, this);
+    std::thread send_thread(&Client::sendThread, this);
+    std::thread recv_thread(&Client::recvThread, this);
+    std::thread connection_check_thread(&Client::connectionCheckThread, this);
 
     send_thread.join();
     recv_thread.join();
+    connection_check_thread.join();
   }
 
   // 发送消息的线程函数
-  void SendThread() {
+  void sendThread() {
     while (true) {
       std::string message{};
       std::getline(std::cin, message);
 
       if (send(sock, message.c_str(), message.size(), 0) < 0) {
         std::cerr << "Send failed: " << strerror(errno) << std::endl;
+        is_connect_ = false;
       }
     }
   }
 
   // 接收消息的线程函数
-  void RecvThread() {
+  void recvThread() {
     char buffer[kBufferSize]{};
     while (true) {
       ssize_t valread = recv(sock, buffer, kBufferSize - 1, 0);
       if (valread <= 0) {
         if (valread == 0) {
           std::cout << "Server closed the connection." << std::endl;
+          is_connect_ = false;
         } else {
           std::cerr << "Recv failed: " << strerror(errno) << std::endl;
         }
@@ -83,19 +91,42 @@ class Client {
       std::cout << "user " << client_fd << ": " << buffer + 4 << std::endl;
     }
   }
+  void connectionCheckThread() {
+    int retry_count = 0;
+    while (true) {
+      if (!is_connect_) {
+        if (retry_count >= kMaxRetries) {
+          std::cerr << "Max retries reached, exiting..." << std::endl;
+          break;
+        }
+        std::cout << "Reconnecting (" << retry_count + 1
+                  << "/" << kMaxRetries << ")..."  << std::endl;
+
+        if (sock > 0) {
+          close(sock);
+        }
+        try {
+          createClientSocket();
+          is_connect_ = true;
+          retry_count = 0;
+          std::cout << "Reconnected to server" << std::endl;
+        } catch (const std::runtime_error& e) {
+          std::cerr << e.what() << std::endl;
+          retry_count++;
+          std::this_thread::sleep_for(std::chrono::seconds(kCheckInterval));
+        }
+      }
+    }
+  }
 
  private:
-  int sock{};
+  int sock{-1};
   char buffer[kBufferSize]{};
+  bool is_connect_{false};
 };
 
 int main() {
   Client client{};
-  try {
-    client.connectServer();
-    client.loop();
-  } catch (const std::exception& e) {
-    std::cerr << e.what() << std::endl;
-  }
+  client.loop();
   return 0;
 }
